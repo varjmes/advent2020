ebb5f0b559ed7b8f649a1a87e8e7a3d15f5682cb
James Spencer <jmes.spncer@gmail.com>
2020-12-07 14:35:21 +0000

    feat: day seven (hell)

---
+++ 7/partTwo.js
@@ -0,0 +1,38 @@
+// --- Day 7: Handy Haversacks ---
+
+const fs = require('fs')
+
+const rules = fs.readFileSync('input.txt').toString().split('\n').filter(entry => entry)
+
+const rulesObj = {}
+
+for (let rule of rules) {
+  const [mainBag, contains] = rule.split(' bags contain ')
+  const innerBags = contains.replace(/\./g, '').split(', ')
+
+  for (let bag of innerBags) {
+    if (bag !== 'no other bags') {
+      const [,number, bagName] = bag.match(/(\d+) (\w+\s\w+)/)
+      if (!rulesObj[mainBag]) rulesObj[mainBag] = {}
+      rulesObj[mainBag][bagName] = number
+    } else {
+      rulesObj[mainBag] = 0
+    }
+  }
+}
+
+const countBags = entries => {
+  let count = 1
+  if (!entries) {
+    return count
+  } else {
+    for (let bag in entries) {
+      let entryCount = parseInt(entries[bag])
+      count += entryCount * countBags(rulesObj[bag]);
+    }
+
+    return count;
+  }
+}
+
+console.log(countBags(rulesObj['shiny gold']) - 1)
